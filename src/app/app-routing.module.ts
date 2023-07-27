27c3fbd5595d0eab2da13f0cc3291b14d4a735af
Änup George <angeorge10@gmail.com>
2023-07-27 05:43:41 +0000

    Init Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {
+    path: 'login',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  { path: '',   
+    redirectTo: '/login',
+    pathMatch: 'full' 
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
