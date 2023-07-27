e35214fb9a20ae9aaff2962a9c510b92a396e674
Änup George <angeorge10@gmail.com>
2023-07-27 17:44:17 +0000

    Adding new routes

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
   }
 ];
 
