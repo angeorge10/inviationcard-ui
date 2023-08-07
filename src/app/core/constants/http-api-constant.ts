cdc7a80ae9291ff46c047efde1e097b98596b573
Änup George <angeorge10@gmail.com>
2023-08-07 14:39:52 +0000

    Updates

--- src/app/core/constants/http-api-constant.ts
+++ src/app/core/constants/http-api-constant.ts
@@ -1,4 +1,4 @@
 export class HttpApiConstant {
-    public static readonly BASE_URL = '';
+    public static readonly BASE_URL = 'localhost:3000';
     public static readonly OVERIRIDES = {} as {[key: string]: {baseUrl: string}};
 }
